d00b3af476bd6991443859fe1c94d93a2d176ed3
flamousz <valflamous@gmail.com>
2023-09-20 23:07:00 +0700

    fetching data in project page

--- src/app/projects/components/ProjectsSection.jsx
+++ src/app/projects/components/ProjectsSection.jsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import proj from "../../../../public/projects/proj1.jpg";
-import proj1a from '../../../../public/projects/proj1a.png'
+import proj1a from "../../../../public/projects/proj1a.png";
+import styles from "../../styles/projects.module.css";
 
 export default function ProjectsSection() {
 	return (
-		<section>
+		<section className={styles.project_section}>
 			<article>
 				<h3>01</h3>
 				<h2>Taco Aw</h2>
@@ -16,6 +17,18 @@ export default function ProjectsSection() {
 				</p>
 			</article>
 			<figure>
+				<Image
+					src={proj}
+					width={500}
+					height={500}
+					alt='potrait of my proj'
+				/>
+				<Image
+					src={proj1a}
+					width={500}
+					height={500}
+					alt='potrait of my proj with second picture'
+				/>
 				<Image
 					src={proj}
 					width={500}
